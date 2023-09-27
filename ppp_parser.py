01b0aeb71bcfbb74e0f2e44f04a4840cab668bdc
Bet20 <55533920+Bet20@users.noreply.github.com>
2023-09-27 21:55:24 +0100

    Added more AST Types

--- ppp_parser.py
+++ ppp_parser.py
@@ -1,18 +1,43 @@
-from re import error
 import pflfp_token as tk
 
 class AstNode:
     def __init__(self):
         pass
 
-class Atom(AstNode):
-    def __init__(self, type: tk.Token, value):
-        self.type = type
-        self.value = value
+class Basic(AstNode):
+    def __init__(self):
         super().__init__()
     
-    def __str__(self) -> str:
-        return self.value
+    def type(self):
+        return self.type
+
+# --- Base types ---
+
+class Int(Basic):
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+
+    def type(self):
+        return tk.T.INT
+
+class Float(Basic):
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+
+    def type(self):
+        return tk.T.FLOAT
+
+class Bool(Basic):
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+
+    def type(self):
+        return tk.T.BOOL
+
+# --- Expressions/Statments ---
 
 class Assignment(AstNode):
     def __init__(self, identifier: str, value: AstNode):
@@ -40,17 +65,20 @@ class Macro(AstNode):
         return super().__str__()
 
 class If(AstNode):
-    def __init__(self, condition: Atom, consequence: AstNode):
+    def __init__(self, condition: Bool, consequence: AstNode):
         self.condition = condition
         self.consequence = consequence
         super().__init__()
 
 class Identifier(AstNode):
     def __init__(self, name):
+        self.name = name
         super().__init__()
 
-def is_type(node: Atom, type: tk.T):
-    return node.type == type
+# ---------------------------
+
+def is_type(node: Basic, type: tk.T):
+    return node.type() == type
 
 def parse(tokens: list[tk.Token], debug: bool = False):
     nodes: list[AstNode] = [] 
@@ -87,8 +115,13 @@ def parse(tokens: list[tk.Token], debug: bool = False):
                     print("error parsing macro, should have an identifier after the block, got " + tokens[idx].literal)
                     exit(-1)
                 nodes.append(Macro(tokens[idx].literal, sub_tok_list))
-            case _:
-                nodes.append(Atom(t.type, t.literal))
+            case tk.T.INT:
+                nodes.append(Int(t.literal))
+            case tk.T.FLOAT:
+                nodes.append(Float(t.literal))
+            case tk.T.BOOL:
+                nodes.append(Bool(t.literal))
+
         idx += 1
         if debug:
             print(nodes[len(nodes) - 1])
